54ddd6bcb89d47004da280d23763e9f6e6428189
Antonio D'Ambrosio <ant05.dambrosio@gmail.com>
2025-05-12 13:03:48 +0200

    small changes

--- RegisterFINAL/Register2/src/main/java/com/esa/moviestar/home/HeaderController.java
+++ RegisterFINAL/Register2/src/main/java/com/esa/moviestar/home/HeaderController.java
@@ -18,7 +18,7 @@ import java.util.ResourceBundle;
 
 public class HeaderController {
     private PopupMenu popupMenu;
-    private final ResourceBundle resources =ResourceBundle.getBundle("com.esa.moviestar.images.svg-paths.general-svg");
+    private final ResourceBundle resources = ResourceBundle.getBundle("com.esa.moviestar.images.svg-paths.general-svg");
 
     @FXML
     private StackPane rootHeader;
@@ -46,7 +46,7 @@ public class HeaderController {
 
     @FXML
     public void initialize() {
-        currentActive=homeButton;
+        currentActive = homeButton;
         currentActive.getStyleClass().remove("surface-transparent");
         currentActive.getStyleClass().add("primary");
         profileImage.setOnMouseClicked(e -> popupMenu.show(profileImage));
@@ -54,14 +54,15 @@ public class HeaderController {
         homeButton.setOnMouseReleased(e -> activeButton(homeButton));
         filmButton.setOnMouseReleased(e -> activeButton(filmButton));
         seriesButton.setOnMouseReleased(e -> activeButton(seriesButton));
-        searchButton.setOnMouseReleased(e -> {});
+        searchButton.setOnMouseReleased(e -> {
+        });
 
         rootHeader.widthProperty().addListener((observable, oldValue, newValue) -> {
             titleImageContainer.setVisible(!(newValue.doubleValue() < 720));
         });
         List<Profile> l = new ArrayList<>();
-        for(int i = 0; i<3; i++ ){
-            Profile n = new Profile(i,"Giacomino","");
+        for (int i = 0; i < 16; i++) {
+            Profile n = new Profile(i, "Giacomino", "", i);
             l.add(n);
         }
         setupPopupMenu(l);
@@ -111,45 +112,82 @@ public class HeaderController {
         popupMenu = new PopupMenu();
 
         // Add menu items
-        HBox settingsItem = new HBox(){{setMinHeight(40.0);setAlignment(Pos.CENTER_LEFT);getStyleClass().addAll("small-item","chips","surface-transparent");}};
-        SVGPath profileIcon = new SVGPath(){{setContent(resources.getString("user")); getStyleClass().add("on-primary");}};
-        Text text = new Text("My Account"){{getStyleClass().addAll("medium-text","on-primary");}};
+        HBox settingsItem = new HBox() {{
+            setMinHeight(40.0);
+            setAlignment(Pos.CENTER_LEFT);
+            getStyleClass().addAll("small-item", "chips", "surface-transparent");
+        }};
+        SVGPath profileIcon = new SVGPath() {{
+            setContent(resources.getString("user"));
+            getStyleClass().add("on-primary");
+        }};
+        Text text = new Text("My Account") {{
+            getStyleClass().addAll("medium-text", "on-primary");
+        }};
         settingsItem.getChildren().addAll(profileIcon, text);
         settingsItem.setOnMouseClicked(e -> settingsClick());
 
         popupMenu.addItem(settingsItem);
         popupMenu.addSeparator();
-        for(Profile i : content){
-            createProfileItem(i.getID(),i.getName(),i.getIcon());
+        for (Profile i : content) {
+            createProfileItem(i.getID(), i.getName(), i.getIcon());
         }
         popupMenu.addSeparator();
-        HBox emailButton = new HBox(){{setMinHeight(40.0);setAlignment(Pos.CENTER); getStyleClass().addAll("small-item","surface-dim-border","chips","surface-transparent");}};
-        SVGPath emailIcon = new SVGPath(){{setContent(resources.getString("email"));getStyleClass().add("on-primary");}};
-        Text emailText = new Text("Change Email"){{getStyleClass().addAll("medium-text","on-primary");}};
+        HBox emailButton = new HBox() {{
+            setMinHeight(40.0);
+            setAlignment(Pos.CENTER);
+            getStyleClass().addAll("small-item", "surface-dim-border", "chips", "surface-transparent");
+        }};
+        SVGPath emailIcon = new SVGPath() {{
+            setContent(resources.getString("email"));
+            getStyleClass().add("on-primary");
+        }};
+        Text emailText = new Text("Change Email") {{
+            getStyleClass().addAll("medium-text", "on-primary");
+        }};
         emailButton.setOnMouseClicked(e -> emailClick());
-        emailButton.getChildren().addAll(emailIcon,emailText);
+        emailButton.getChildren().addAll(emailIcon, emailText);
         popupMenu.addItem(emailButton);
 
     }
-    private void createProfileItem(int id,String name, Group profileIcon){
-        HBox item = new HBox(){{setMinHeight(40.0);setAlignment(Pos.CENTER_LEFT);getStyleClass().addAll("small-item","chips","surface-transparent");}};
-        Text text = new Text(name){{getStyleClass().addAll("medium-text","on-primary");}};
+
+    private void createProfileItem(int id, String name, Group profileIcon) {
+        profileIcon.setScaleX(1.3);
+        profileIcon.setScaleY(1.3);
+        HBox item = new HBox() {{
+            setMinHeight(40.0);
+            setAlignment(Pos.CENTER_LEFT);
+            getStyleClass().addAll("small-item", "chips", "surface-transparent");
+        }};
+        Text text = new Text(name) {{
+            getStyleClass().addAll("medium-text", "on-primary");
+        }};
         item.getChildren().addAll(profileIcon, text);
         // Item click handling moved to controller via callback
         item.setOnMouseClicked(e -> profileClick(id));
         popupMenu.addItem(item);
 
     }
+
     private void profileClick(int itemId) {
         // Handle menu selections based on ID
-        System.out.println("selected profile n:"+ itemId);
+        System.out.println("selected profile n:" + itemId);
     }
+
     private void settingsClick() {
         System.out.println("settings button clicked");
         // Handle email change logic here
     }
+
     private void emailClick() {
         System.out.println("Change email button clicked");
         // Handle email change logic here
+    }
+
+    public void setProfileIcon(Group icon) {
+        profileImage.getChildren().clear();
+        icon.setScaleX(2.286);
+        icon.setScaleY(2.286);
+        profileImage.getChildren().add(icon);
     }
 }
