5d4b5352f271028425825427d0a03172f75281e7
eug <grecoe02@gmail.com>
2025-05-20 17:20:35 +0200

    aggiunto un po di stile alla parte iniziale dei settings

--- RegisterFINAL/Register2/src/main/java/com/esa/moviestar/Settings/SettingsApplication.java
+++ RegisterFINAL/Register2/src/main/java/com/esa/moviestar/Settings/SettingsApplication.java
@@ -6,14 +6,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 public class SettingsApplication extends Application {
 
-
+    private final ResourceBundle resourceBundle = ResourceBundle.getBundle("com.esa.moviestar.images.svg-paths.general-svg");
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/esa/moviestar/Settings_FXML/settings-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/esa/moviestar/Settings_FXML/settings-view.fxml"),resourceBundle);
         Scene scene = new Scene(fxmlLoader.load());
         primaryStage.setTitle("Titolo della finestra");
         primaryStage.setMaximized(true);
